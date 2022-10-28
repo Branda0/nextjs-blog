56e8112fb79456f07fb63fc8f9ddb233b74261d5
Branda0 <g.brandao.dev@gmail.com>
2022-10-28 21:39:41 +0200

    nextjs blog tutorial

---
+++ pages/index.tsx
@@ -0,0 +1,98 @@
+import React from "react";
+import Head from "next/head";
+import Link from "next/link";
+import { useState, useEffect } from "react";
+
+import { getSortedPostsData } from "../lib/posts";
+
+import Layout, { siteTitle } from "../components/layout";
+import Date from "../components/date";
+import utilStyles from "../styles/utils.module.css";
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: { allPostsData },
+  };
+}
+
+export default function Home({ allPostsData }) {
+  const [data, setData] = useState<number>();
+
+  const userMap = new Map<string, { id: number; status: string }>([
+    ["theo", { id: 7, status: "online" }],
+    ["lea", { id: 10, status: "offline" }],
+  ]);
+
+  const user = userMap.get("lea");
+  // console.log(userMap.get("theo"));
+  // console.log(user);
+  const array = [1, 2, 3];
+  // console.log(...userMap);
+  [...userMap].map((user) => console.log(user[0], user[1]));
+  function multiply(params: number, multiplier: number) {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(params * multiplier);
+      }, 3000);
+    });
+
+    // setTimeout(() => {
+    //   console.log("server fetching");
+    //   return params * multiplier;
+    //   console.log("ok");
+    // }, 3000);
+  }
+
+  useEffect(() => {
+    let isSubscribed = true;
+
+    const fetchData = async () => {
+      try {
+        const response = (await multiply(3, 4)) as number;
+        console.log({ response: response });
+        if (isSubscribed) setData(response);
+      } catch (error) {
+        console.log(error);
+      }
+      console.log("resolution ou rejet");
+    };
+
+    fetchData();
+    console.log("fetching");
+
+    return () => {
+      isSubscribed = false;
+      console.log("clean up");
+    };
+  }, []);
+
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>[Your Self Introduction]</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{" "}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>{title}</Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  );
+}
